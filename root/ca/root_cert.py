995e00c6266a493902ded2c7918a2eb6f2c877ba
Dan Glade <dan.glade@oracle.com>
2018-03-07 23:35:27 -0600

    fix path, add input and conditionals

--- root/ca/root_cert.py
+++ root/ca/root_cert.py
@@ -1,60 +1,29 @@
-
+import os
 import subprocess
 
+
+def input_yes_no(query):
+    answer = ''
+    while answer.lower() not in ['yes', 'y', 'no', 'n']:
+        answer = raw_input('%s (y/n): ' % query)
+    return answer[0] == 'y'
+
+
 if __name__ == '__main__':
-    root_key_name = raw_input('Enter a name for the root private key: ')
-    key_length = 0
-    while key_length != 2048 and key_length != 4096:
-        try:
-            key_length = raw_input('Enter the desired key length (2048 or 4096): ')
-            key_length = int(key_length)
-        except Exception as e:
-            print('Invalid key length or format, choose from integgers 2048 or 4096.')
-    print('generating new root private key with name {} and length {}'.format(root_key_name, key_length))
 
-    subprocess.call(['./root_cert.sh'])
-# openssl genrsa -aes256 -out private/ca.key.pem 4096
+    script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'root_cert.sh')
 
-# echo 'generating new root certificate'
-# openssl req -config openssl.cnf -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem
+    gen_private = input_yes_no('Generate a new private key/cert?')
+    gen_intermediate = input_yes_no('Generate a new intermediate key/cert?')
+    gen_server_cert = input_yes_no('Generate a new server key/cert?')
+    gen_client_cert = input_yes_no('Generate a new client key/cert?')
 
-# echo 'verifying root certificate'
-# openssl x509 -noout -text -in certs/ca.cert.pem
-
-# echo 'generating new intermediate private key'
-# openssl genrsa -aes256 -out intermediate/private/intermediate.key.pem 4096
-
-# echo 'generating new intermediate csr'
-# openssl req -config intermediate/openssl.cnf -new -sha256 -key intermediate/private/intermediate.key.pem -out intermediate/csr/intermediate.csr.pem
-
-# echo 'generating new intermediate certificate'
-# openssl req -config intermediate/openssl.cnf -new -sha256 -key intermediate/private/intermediate.key.pem -out intermediate/csr/intermediate.csr.pem
-
-# echo 'verify intermediate certificate'
-# openssl x509 -noout -text -in intermediate/certs/intermediate.cert.pem
-# openssl verify -CAfile certs/ca.cert.pem intermediate/certs/intermediate.cert.pem
-
-# echo 'creating cerficate chain file'
-# cat intermediate/certs/intermediate.cert.pem certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
-
-# echo 'generating a new server-side private key'
-# openssl genrsa -out intermediate/private/final.key.pem 2048
-
-# echo 'generating a new server-side csr'
-# openssl req -config intermediate/openssl.cnf -key intermediate/private/final.key.pem -new -sha256 -out intermediate/csr/final.csr.pem
-
-# echo 'generating a new server-side certificate'
-# openssl ca -config intermediate/openssl.cnf -extensions server_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/server.final.cert.pem
-
-# echo 'generating a new client certificate'
-# openssl ca -config intermediate/openssl.cnf -extensions usr_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/client.final.cert.pem
-
-# echo 'verify server-side certificate'
-# openssl x509 -noout -text -in intermediate/certs/server.final.cert.pem
-# openssl verify -CAfile intermediate/certs/ca-chain.cert.pemintermediate/certs/server.final.cert.pem
-
-# echo 'verify client certificate'
-# echo 'verify server-side certificate'
-# openssl x509 -noout -text -in intermediate/certs/client.final.cert.pem
-# openssl verify -CAfile intermediate/certs/ca-chain.cert.pemintermediate/certs/client.final.cert.pem
-
+    subprocess.call(
+        [
+            script,
+            str(gen_private),
+            str(gen_intermediate),
+            str(gen_server_cert),
+            str(gen_client_cert)
+        ]
+    )
--- root/ca/root_cert.sh
+++ root/ca/root_cert.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 
-echo 'generating new root private key with length 4096 at private/ca.key.pem'
-openssl genrsa -aes256 -out private/ca.key.pem 4096
+MAKE_ROOT=$1
+MAKE_INT=$2
+MAKE_SERVER=$3
+MAKE_CLIENT=$4
 
-echo 'generating new root certificate at certs/ca.cert.pem'
-openssl req -config openssl.cnf -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem
+if [ $MAKE_ROOT == 'True' ]; then
+	echo 'generating new root private key with length 4096 at private/ca.key.pem'
+	openssl genrsa -aes256 -out private/ca.key.pem 4096
 
-echo 'verifying root certificate'
-openssl x509 -noout -text -in certs/ca.cert.pem
+	echo 'generating new root certificate at certs/ca.cert.pem'
+	openssl req -config openssl.cnf -key private/ca.key.pem -new -x509 -days 7300 -sha256 -extensions v3_ca -out certs/ca.cert.pem
 
-echo 'generating new intermediate private key with length 4096 at intermediate/private/intermediate.key.pem'
-openssl genrsa -aes256 -out intermediate/private/intermediate.key.pem 4096
+	echo 'verifying root certificate'
+	openssl x509 -noout -text -in certs/ca.cert.pem
+fi
 
-echo 'generating new intermediate csr'
-openssl req -config intermediate/openssl.cnf -new -sha256 -key intermediate/private/intermediate.key.pem -out intermediate/csr/intermediate.csr.pem
+if [ $MAKE_INT == 'True' ]; then
+	echo 'generating new intermediate private key with length 4096 at intermediate/private/intermediate.key.pem'
+	openssl genrsa -aes256 -out intermediate/private/intermediate.key.pem 4096
 
-echo 'generating new intermediate certificate'
-openssl ca -config openssl.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in intermediate/csr/intermediate.csr.pem -out intermediate/certs/intermediate.cert.pem
+	echo 'generating new intermediate csr'
+	openssl req -config intermediate/openssl.cnf -new -sha256 -key intermediate/private/intermediate.key.pem -out intermediate/csr/intermediate.csr.pem
 
-echo 'verify intermediate certificate'
-openssl x509 -noout -text -in intermediate/certs/intermediate.cert.pem
-openssl verify -CAfile certs/ca.cert.pem intermediate/certs/intermediate.cert.pem
+	echo 'generating new intermediate certificate'
+	openssl ca -config openssl.cnf -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in intermediate/csr/intermediate.csr.pem -out intermediate/certs/intermediate.cert.pem
 
-echo 'creating cerficate chain file'
-cat intermediate/certs/intermediate.cert.pem certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
+	echo 'verify intermediate certificate'
+	openssl x509 -noout -text -in intermediate/certs/intermediate.cert.pem
+	openssl verify -CAfile certs/ca.cert.pem intermediate/certs/intermediate.cert.pem
 
-echo 'generating a new server-side private key'
-openssl genrsa -out intermediate/private/final.key.pem 2048
+	echo 'creating cerficate chain file'
+	cat intermediate/certs/intermediate.cert.pem certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
+fi
 
-echo 'generating a new server-side csr'
-openssl req -config intermediate/openssl.cnf -key intermediate/private/final.key.pem -new -sha256 -out intermediate/csr/final.csr.pem
+if [ $MAKE_SERVER == 'True' ]; then
+	echo 'generating a new server-side private key'
+	openssl genrsa -out intermediate/private/final.key.pem 2048
 
-echo 'generating a new server-side certificate'
-openssl ca -config intermediate/openssl.cnf -extensions server_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/server.final.cert.pem
+	echo 'generating a new server-side csr'
+	openssl req -config intermediate/openssl.cnf -key intermediate/private/final.key.pem -new -sha256 -out intermediate/csr/final.csr.pem
 
-echo 'generating a new client certificate'
-openssl ca -config intermediate/openssl.cnf -extensions usr_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/client.final.cert.pem
+	echo 'generating a new server-side certificate'
+	openssl ca -config intermediate/openssl.cnf -extensions server_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/server.final.cert.pem
 
-echo 'verify server-side certificate'
-openssl x509 -noout -text -in intermediate/certs/server.final.cert.pem
-openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/server.final.cert.pem
+	echo 'verify server-side certificate'
+	openssl x509 -noout -text -in intermediate/certs/server.final.cert.pem
+	openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/server.final.cert.pem
+fi
 
-echo 'verify client certificate'
-openssl x509 -noout -text -in intermediate/certs/client.final.cert.pem
-openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/client.final.cert.pem
+if [ $MAKE_CLIENT == 'True' ]; then
+	# echo 'generating a new client csr'
+	# openssl req -config intermediate/openssl.cnf -key intermediate/private/final.key.pem -new -sha256 -out intermediate/csr/final.csr.pem
 
+	echo 'generating a new client certificate'
+	openssl ca -config intermediate/openssl.cnf -extensions usr_cert -days 375 -notext -md sha256 -in intermediate/csr/final.csr.pem -out intermediate/certs/client.final.cert.pem
+
+	echo 'verify client certificate'
+	openssl x509 -noout -text -in intermediate/certs/client.final.cert.pem
+	openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/client.final.cert.pem
+fi
